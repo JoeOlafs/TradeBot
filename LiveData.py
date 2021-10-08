849d9485022fab66b696cdaa30c3c3ce29fc017f
JoeOlafs <jaolafsson@hotmail.com>
2021-10-08 04:01:58 -0700

    GettingValue_Jupyter_AsyncFunctions

--- LiveData.py
+++ LiveData.py
@@ -1,7 +1,28 @@
 import keys
 import pandas as pd
 import sqlalchemy
+import asyncio
 from binance.client import Client
-from binance import BinanceSocketManager
+from binance import AsyncClient, BinanceSocketManager
 
-client = Client(keys.api_key,keys.secret_key)
+async def main():
+     client = await AsyncClient.create(keys.api_key, keys.secret_key)
+     bsm = BinanceSocketManager(client)
+     trade_socket = bsm.trade_socket('ETHUSDC')
+     async with trade_socket as tscm:
+          while True:
+               msg = await tscm.recv()
+               print(createframe(msg))
+     await client.close_connection()
+
+def createframe(msg):
+     df = pd.DataFrame([msg])
+     df = df.loc[:,['s', 'E', 'p']]
+     df.columns = ['Symbol', 'Time', 'Price']
+     df.Price = df.Price.astype(float)
+     df.Time = pd.to_datetime(df.Time, unit='ms')
+     return df
+
+if __name__ == '__main__':
+     loop = asyncio.get_event_loop()
+     loop.run_until_complete(main())
